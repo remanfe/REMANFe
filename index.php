301a50a8ac5970f2929593b95ce00d4a4a3abc54
ldor1 <ldor1@DESKTOP-GC64TSG>
2019-09-01 14:50:14 -0300

    Acidionando arquivos do projeto completo

--- index.php
+++ index.php
@@ -1,18 +1,153 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
+<html lang="pt-br">
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>REMANFe | Dashboard</title>
+        <!-- Diga ao navegador para responder à largura da tela -->
+        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+        <!-- Bootstrap 3.3.7 -->
+        <link rel="stylesheet" href="components/bootstrap/dist/css/bootstrap.min.css">
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="components/font-awesome/css/font-awesome.min.css">
+        <!-- Theme style -->
+        <link rel="stylesheet" href="css/AdminLTE.min.css">
+        <!-- Skin AdminLTE. -->
+        <!--<link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">-->
+        <link rel="stylesheet" href="css/skins/skin-blue.min.css">
     </head>
-    <body>
-        <h1>Hello World!!</h1>
-        <?php
-        // put your code here
-        ?>
+    <body class="hold-transition skin-blue sidebar-mini">
+        <div class="wrapper">
+            <header class="main-header">
+                <!-- Logo -->
+                <a href="#" class="logo">
+                    <!-- mini logotipo para barra lateral 50x50 pixels -->
+                    <span class="logo-mini"><b>RMF</b></span>
+                    <!-- logotipo para estado regular e dispositivos móveis -->
+                    <span class="logo-lg"><b>REMANFe</b></span>
+                </a>
+                <!-- Header Navbar: o estilo pode ser encontrado em header.less -->
+                <nav class="navbar navbar-static-top">
+                    <!-- Sidebar toggle button-->
+                    <a class="sidebar-toggle" data-toggle="push-menu" role="button">
+                        <span class="sr-only">Menu lateral</span>
+                    </a>
+                    <div class="navbar-custom-menu">
+                        <ul class="nav navbar-nav">
+                            <!-- User Account: o estilo pode ser encontrado em dropdown.less -->
+                            <li class="dropdown user user-menu">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                    <span class="hidden-xs">REMANFe</span>
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <!-- Imagem do usuário -->
+                                    <li class="user-header">
+                                        <img src="dist/img/avatar5.png" class="img-circle" alt="Imagem de Perfil">
+                                        <p>REMANFe - Admin</p>
+                                    </li>
+                                    <!-- Menu Footer-->
+                                    <li class="user-footer">
+                                        <div class="pull-left">
+                                            <a href="#" class="btn btn-default btn-flat">Dados cadastrais</a>
+                                        </div>
+                                        <div class="pull-right">
+                                            <a href="#" class="btn btn-default btn-flat">Sair</a>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+                <!-- /.header -->
+            </header>
+            <!-- Coluna do lado esquerdo. Contém o logotipo e a barra lateral. -->
+            <aside class="main-sidebar">
+                <!-- sidebar: estilo pode ser encontrado em sidebar.less -->
+                <section class="sidebar">
+                    <!-- Sidebar user panel -->
+                    <div class="user-panel">
+                        <div class="pull-left image">
+                            <img src="dist/img/avatar5.png" class="img-circle" alt="Imagem de Perfil">
+                        </div>
+                        <div class="pull-left info">
+                            <p>Usuário REMANFe</p>
+                        </div>
+                    </div>
+                    <!-- Sidebar menu: : style can be found in sidebar.less -->
+                    <ul class="sidebar-menu" data-widget="tree">
+                        <li class="header">MENU</li>
+                        <li class="treeview">
+                            <a href="#">
+                                <i class="fa fa-user-plus"></i><span>CADASTRAR</span>
+                                <span class="pull-right-container">
+                                    <i class="fa fa-angle-left pull-right"></i>
+                                </span>
+                            </a>
+                            <ul class="treeview-menu">
+                                <li><a href="#"><i class="fa fa-circle-o"></i>Administrador</a></li>
+                                <li><a href="#"><i class="fa fa-circle-o"></i>Contador</a></li>
+                                <li><a href="#"><i class="fa fa-circle-o"></i>Empresa</a></li>
+                            </ul>
+                        </li>
+                        <li class="treeview">
+                            <a href="#">
+                                <i class="fa fa-edit"></i><span>NF-e (Nota Fiscal Eletrônica)</span>
+                                <span class="pull-right-container">
+                                    <i class="fa fa-angle-left pull-right"></i>
+                                </span>
+                            </a>
+                            <ul class="treeview-menu">
+                                <li><a href="#"><i class="fa fa-upload"></i>UPLOAD NF-e</a></li>
+                                <li><a href="#"><i class="fa fa-download"></i>DOWNLOAD NF-e</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fa fa-print"></i><span>GERAR DANFe</span>
+                            </a>
+                        </li>
+                    </ul>
+                </section>
+                <!-- /.sidebar -->
+            </aside>
+
+            <!-- Content Wrapper. Contém o conteúdo da página -->
+            <div class="content-wrapper">
+                <!-- Content Header (Page header) -->
+                <section class="content-header">
+                    <h1>
+                        Dashboard
+                        <small>Painel de Controle</small>
+                    </h1>
+                    <ol class="breadcrumb">
+                        <li><a href="#"><i class="fa fa-dashboard"></i> Inicio</a></li>
+                        <li class="active">Dashboard</li>
+                    </ol>
+                </section>
+                <section class="content-header">
+                    HELLO WORLD!
+                </section>
+            </div>
+            <!-- /.content-wrapper -->
+            <footer class="main-footer">
+                <div class="pull-right hidden-xs">
+                    <b>Version</b> Beta
+                </div>
+                <strong>Copyright &copy; 2019 REMANFe.</strong> Todos os direitos reservados.
+            </footer>
+
+            <!-- Adicione o sidebar. Este div deve ser colocado imediatamente após a barra lateral de controle. -->
+            <div class="control-sidebar-bg"></div>
+        </div>
+
+        <!-- jQuery 3 -->
+        <script src="components/jquery/dist/jquery.min.js"></script>
+        <!-- jQuery UI 1.11.4 -->
+        <script src="components/jquery-ui/jquery-ui.min.js"></script>
+        <!-- Bootstrap 3.3.7 -->
+        <script src="components/bootstrap/dist/js/bootstrap.min.js"></script>
+        <!-- AdminLTE App -->
+        <script src="js/adminlte.min.js"></script>
     </body>
 </html>
---
+++ login.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>REMANFe Login</title>
+        <meta charset="UTF-8">
+        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+        <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+        <link rel="stylesheet" type="text/css" href="css/util.css">
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+        <div class="limiter">
+            <div class="container-login100" style="background-image: url('images/fundo.png')">
+                <div class="wrap-login100 p-t-30 p-b-50">
+                    <span class="login100-form-title p-b-41">
+                        REMANF<span style="text-transform: lowercase;">e</span> Login
+                    </span>
+                    <form autocomplete="off" class="login100-form validate-form p-b-30 p-t-5">
+
+                        <div class="wrap-input100 validate-input" data-validate = "Informe o usuário">
+                            <input class="input100" type="text" name="usuario" placeholder="Usuário">
+                            <span class="focus-input100" data-placeholder="&#xe82a;"></span>
+                        </div>
+
+                        <div class="wrap-input100 validate-input" data-validate="Informe a senha">
+                            <input class="input100" type="password" name="senha" placeholder="Senha">
+                            <span class="focus-input100" data-placeholder="&#xe80f;"></span>
+                        </div>
+
+                        <div class="container-login100-form-btn m-t-30">
+                            <button class="login100-form-btn adjust-button">
+                                Login
+                            </button>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <script src="js/jquery/jquery-3.2.1.min.js"></script>
+        <script src="js/main.js"></script>
+    </body>
+</html>
